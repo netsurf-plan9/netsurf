9c3982659365083e47779f2aee279fb350313b25
phil9 <telephil9@gmail.com>
2021-02-14 11:14:54 +0000

    frontends/plan9: remove forgotten debug code in previous commit

--- frontends/plan9/drawui/entry.c
+++ frontends/plan9/drawui/entry.c
@@ -87,7 +87,6 @@ void dentry_set_text(dentry *entry, const char *text)
 	}
 	strncpy(entry->text, text, l);
 	entry->text[l] = 0;
-	entry->off = 0;
 	entry->len = l;
 	entry->pos = entry->len;
 	entry->pos2 = entry->pos;
@@ -163,7 +162,7 @@ static bool is_separator(char c)
 static void entry_click_sel(dentry *entry)
 {
 	int s, e;
-DBG("entry_click_sel");
+
 	if (entry->pos == 0)
 		entry->pos2 = entry->len;
 	else if (entry->pos == entry->len)
